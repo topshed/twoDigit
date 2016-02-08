76df21b87dc613880434db6339a03e0b036f651b
Richard Hayler <top_shed@yahoo.com>
2016-02-08 21:43:17 +0000

    modified setup.py again

--- setup.py
+++ setup.py
@@ -3,11 +3,10 @@ from distutils.core import setup
 setup(name='twoDigit',
       packages = ['twoDigit'],
       version='1.0',
-      description='Produces 2 digit output for 8x8 LED matrices like Raspberry Pi SenseHAT'
-      author='Richard Hayler'
-      author_email='top_shed@yahoo.com'
+      description='Produces 2 digit output for 8x8 LED matrices like Raspberry Pi SenseHAT',
+      author='Richard Hayler',
+      author_email='top_shed@yahoo.com',
       url='https://www.python.org/sigs/distutils-sig/',
       download_url='https://github.com/topshed/twoDigit/tarball/0.1',
       packages=['distutils', 'distutils.command'],
-      download_url = '' 
      )
